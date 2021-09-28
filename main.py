bbe59f959629d55305df1d8f498306280c41bbc9
Tariq <tariq9853@gmail.com>
2021-09-28 19:16:00 +0200

    commit

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 #!/usr/bin/env python
 
+import time
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
+import mysql.connector
 
+db = mysql.connector.connect(
+  host="localhost",
+  user="tariq",
+  passwd="Sitecom12",
+  database="attendancesystem"
+)
+
+cursor = db.cursor()
 reader = SimpleMFRC522()
+
 try:
-        id, text = reader.read()
-        print(id)
-        print(text)
+  while True:
+    id, text = reader.read()
+
+    if cursor.rowcount >= 1:
+      overwrite = input("Overwite (Y/N)? ")
+      if overwrite[0] == 'Y' or overwrite[0] == 'y':
+        time.sleep(1)
+        sql_insert = "UPDATE users SET name = %s WHERE rfid_uid=%s"
+      else:
+        continue;
+    else:
+      sql_insert = "INSERT INTO users (name, rfid_uid) VALUES (%s, %s)"
+    new_name = input("Name: ")
+
+    cursor.execute(sql_insert, (new_name, id))
+
+    db.commit()
+    time.sleep(2)
 finally:
-        GPIO.cleanup()
+  GPIO.cleanup()
